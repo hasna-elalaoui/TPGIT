d58ba22b6de438dd347acd3223f8bb11db53a4ee
ELALAOUI Hasna <hasna.elalaoui@live.fr>
2021-03-01 12:06:13 +0100

    Add a model: User Class

--- src/test/Main.java
+++ src/test/Main.java
@@ -5,6 +5,7 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("ceci est un test");
+		System.out.println("ceci est un 2eme test");
 	}
 
 }
